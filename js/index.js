8118409c820c17785cfee82520d0ef00f830f2f2
adapptalo <adapptalo@gmail.com>
2014-03-19 20:24:20 +0100

    test map app

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+  var app = new kendo.mobile.Application(document.body);
 app = {
   // Application Constructor
   initialize: function () {
